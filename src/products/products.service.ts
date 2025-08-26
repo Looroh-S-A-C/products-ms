64661aa4d2ee390ccf2dddee63be038bbea5dd78
jessfigueroa <87987208+jessfigueroa@users.noreply.github.com>
2025-08-26 17:56:54 -0500

    feat: [Jess Figueroa] product Service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,5 @@
 import {
+  HttpStatus,
   Injectable,
   Logger,
   NotFoundException,
@@ -8,6 +9,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -55,7 +57,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
       },
     });
     if (!product) {
-      throw new NotFoundException(`Product with id ${id} not found`);
+      throw new RpcException({
+        statusCode: HttpStatus.NOT_FOUND,
+        message: `Product with id ${id} not found`,
+      });
     }
     return product;
   }
